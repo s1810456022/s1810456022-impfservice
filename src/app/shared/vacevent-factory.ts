b8b35693eb8e3ff053c63a2a0c5e2da499a0b9d1
s1810456022 <s1810456022@students.fh-hagenberg.at>
2021-04-16 17:15:41 +0200

    2 files modified

--- src/app/shared/vacevent-factory.ts
+++ src/app/shared/vacevent-factory.ts
@@ -2,7 +2,7 @@ import { DatePipe } from '@angular/common';
 import { Vacevent } from './vacevent';
 export class VaceventFactory {
  static empty(): Vacevent {
-   return new Vacevent(null,new Date(),new DatePipe(''),new DatePipe(''), 0, 0, [{id: 0, firstName: '', lastName: '', admin: false, gender: '', dateOfBirth: new Date(), svnr: 0, phoneNumber: 0, street: '', houseNumber: '', postalCode: 0, city: '', vacStatus: false, email: '', password: '', vacevent_id: 0}]);
+   return new Vacevent(null,new Date(),new DatePipe(''),new DatePipe(''), 0, 0, [{id: 0, firstName: '', lastName: '', admin: false, gender: '', dateOfBirth: new Date(), svnr: 0, phoneNr: 0, street: '', houseNumber: '', postalCode: 0, city: '', vacStatus: false, email: '', password: '', vacevent_id: 0}]);
  }
  static fromObject(rawVacevent: any): Vacevent {
    return new Vacevent(
--- src/app/shared/vacevent.ts
+++ src/app/shared/vacevent.ts
@@ -1,11 +1,11 @@
-import { Time } from "@angular/common";
+import { DatePipe} from "@angular/common";
 import { User } from "./user";
 
 export class Vacevent {
   constructor(public id:number, 
   public date:Date, 
-  public startTime:Time, 
-  public endTime:Time, 
+  public startTime:DatePipe, 
+  public endTime:DatePipe, 
   public maxVac:number,
   public vaclocation_id:number,
   public users?:User[]) {}
