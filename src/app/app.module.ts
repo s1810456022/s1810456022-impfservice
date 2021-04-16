3bbc5dc1a838ff6e7b8bf658b970882dcfe83aa2
s1810456022 <s1810456022@students.fh-hagenberg.at>
2021-04-16 16:38:22 +0200

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { VaccinationService } from './shared/vaccination.service';
+import { UserService } from './shared/user.service';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports:      [ BrowserModule, ReactiveFormsModule, AppRoutingModule, HttpClientModule ],
+  declarations: [ AppComponent, HomeComponent ],
+  bootstrap:    [ AppComponent ],
+  providers:    [ VaccinationService, UserService]
 })
 export class AppModule { }
