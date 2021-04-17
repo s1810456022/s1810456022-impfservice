5fca7245aab055543e7487d9e8a91b515851f10b
s1810456022 <s1810456022@students.fh-hagenberg.at>
2021-04-17 21:55:59 +0200

    4 files modified

--- src/app/vacevent-state-item/vacevent-state-item.component.ts
+++ src/app/vacevent-state-item/vacevent-state-item.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Vacevent } from '../shared/vacevent';
 
 @Component({
-  selector: 'li.vac-vacevent-state-item',
+  selector: 'span.vac-vacevent-state-item',
   templateUrl: './vacevent-state-item.component.html'
 })
 export class VaceventStateItemComponent implements OnInit {
