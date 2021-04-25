417229c98542db10d2cdea6f4d2a9010c32cc994
s1810456022 <s1810456022@students.fh-hagenberg.at>
2021-04-25 16:25:29 +0200

    3 files modified

--- src/app/vacevent-state/vacevent-state.component.ts
+++ src/app/vacevent-state/vacevent-state.component.ts
@@ -1,7 +1,10 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Vacevent } from '../shared/vacevent';
 import { VaceventService } from '../shared/vacevent.service';
+import { Vaclocation } from '../shared/vaclocation';
+import { VaclocationService } from '../shared/vaclocation.service';
 
 @Component({
   selector: 'vac-vacevent-state',
@@ -13,16 +16,37 @@ export class VaceventStateComponent implements OnInit {
   state:string = "";
 
   admin:boolean = true;
+  vaclocationForm: FormGroup;
+  vaclocation:Vaclocation[];
+  selected:boolean = true;
 
 
-  constructor(private vac:VaceventService, private route:ActivatedRoute, private router:Router) { }
+  constructor(private vac:VaceventService, private route:ActivatedRoute, private router:Router, private vacloc:VaclocationService, private fb:FormBuilder) { }
 
 
   ngOnInit() {
     const params = this.route.snapshot.params;
     let state1 = params['state'];
-    this.vac.getByState(params['state']).subscribe(res => this.vacevents = res);
+    this.vac.getByState(state1).subscribe(res => this.vacevents = res);
     this.state = state1;
+
+    this.vacloc.getLocationByState(state1).subscribe(vaclocation => {
+        this.vaclocation = vaclocation;
+        this.initVaclocationsSelect();
+      });
+    this.initVaclocationsSelect();
+  }
+
+  initVaclocationsSelect(){
+    this.vaclocationForm = this.fb.group({
+      vaclocations: this.vaclocation
+    });
+  }
+
+  onChange(e: Event){
+    let value = (<HTMLInputElement>e.target).value;
+    console.log(value);
+    
   }
 
 }
