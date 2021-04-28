9ea38ee1f4d5a8c0cf7fddd74d1aa5b8428a82ed
s1810456022 <s1810456022@students.fh-hagenberg.at>
2021-04-28 21:36:50 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,5 @@
-import { Component, VERSION } from '@angular/core';
+import { Component} from '@angular/core';
 import { AuthenticationService } from './shared/authentication.service';
-import { Vacevent } from './shared/vacevent';
-import { VaceventFactory } from './shared/vacevent-factory';
-import { VaceventService } from './shared/vacevent.service';
 
 @Component({
   selector: 'vac-root',
@@ -11,45 +8,9 @@ import { VaceventService } from './shared/vacevent.service';
 })
 export class AppComponent {
 
-  admin:string = "";
-  firstName:string = "";
-  lastName:string = "";
-  vacevent_date:Date = new Date();
-  vacevent_startTime:Date = new Date();
-  vacevent_endTime:Date = new Date();
-  vacevent_location:string = "";
-  vacevent:Vacevent = VaceventFactory.empty();
-  booked:boolean = false;
-  hasBooked:boolean = false;
 
+  constructor(private authService:AuthenticationService){}
 
-  constructor(private authService:AuthenticationService, private vac:VaceventService){}
-
-  ngOnInit() {
-    this.admin = localStorage.getItem("admin");
-
-    if(this.isLoggedIn() && localStorage.getItem("admin")!== "1"){
-      this.firstName = localStorage.getItem("firstName");
-      this.lastName = localStorage.getItem("lastName");
-      
-      if(localStorage.getItem("vacevent_id") !== "null"){
-        this.hasBooked = true;
-        this.booked = true;
-        this.vac.getSingle(Number(localStorage.getItem("vacevent_id"))).subscribe(vacevent => {
-          this.vacevent = vacevent;
-          this.vacevent_date = this.vacevent.date;
-          this.vacevent_startTime = this.vacevent.startTime;
-          this.vacevent_endTime = this.vacevent.endTime;
-          console.log(this.vacevent.vaclocation.name);
-          this.vacevent_location = this.vacevent.vaclocation.name;
-        });
-      } else {
-        this.vacevent_location = "nicht angemeldet";
-      }
-      
-    }
-    
-  }
 
   isLoggedIn(){
     return this.authService.isLoggedIn();
