a01f8e1385865b9b9ba9118481ab70ff2e6b0bed
s1810456022 <s1810456022@students.fh-hagenberg.at>
2021-04-30 09:51:56 +0200

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
@@ -13,7 +13,7 @@ import { VaceventDetailsComponent } from './vacevent-details/vacevent-details.co
 import { VaceventDetailsFormComponent } from './vacevent-details-form/vacevent-details-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {ToastrModule} from "ngx-toastr";
-import { DatePipe} from '@angular/common';
+import { DatePipe, registerLocaleData} from '@angular/common';
 import { VaclocationService } from './shared/vaclocation.service';
 import {MomentModule} from 'ngx-moment';
 import { LoginComponent } from './login/login.component';
@@ -21,6 +21,9 @@ import { AuthenticationService } from './shared/authentication.service';
 import { TokenInterceptorService } from './shared/token-interceptor.service';
 import { JwtInterceptorService } from './shared/jwt.interceptor.service';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import localeDe from '@angular/common/locales/de';
+
+registerLocaleData(localeDe);
 
 @NgModule({
   imports:      [ BrowserModule, ReactiveFormsModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule, ToastrModule.forRoot(), MomentModule ],
@@ -36,6 +39,9 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
     provide:HTTP_INTERCEPTORS,
     useClass: JwtInterceptorService,
     multi: true
+  },
+  {
+    provide: LOCALE_ID, useValue:'de'
   }
   ]
 })
