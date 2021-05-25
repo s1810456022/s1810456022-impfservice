3423f3c76890f6993b3dbbfc1f964750fa45d16b
s1810456022 <s1810456022@students.fh-hagenberg.at>
2021-05-25 16:32:15 +0200

    3 files modified

--- src/app/vacevent-details-form/vacevent-details-form.component.ts
+++ src/app/vacevent-details-form/vacevent-details-form.component.ts
@@ -3,12 +3,12 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Vacevent } from '../shared/vacevent';
 import { VaceventService } from '../shared/vacevent.service';
-import { DatePipe} from '@angular/common';
+import { DatePipe } from '@angular/common';
 import { VaclocationService } from '../shared/vaclocation.service';
 import { Vaclocation } from '../shared/vaclocation';
 import { VaceventFactory } from '../shared/vacevent-factory';
-import { ToastrService} from "ngx-toastr";
-import { VaceventDetailsFormErrorMessages } from "./vacevent-details-form-error-messages";
+import { ToastrService } from 'ngx-toastr';
+import { VaceventDetailsFormErrorMessages } from './vacevent-details-form-error-messages';
 import moment from 'moment';
 
 @Component({
@@ -16,25 +16,30 @@ import moment from 'moment';
   templateUrl: './vacevent-details-form.component.html'
 })
 export class VaceventDetailsFormComponent implements OnInit {
-
-  id:bigint;
+  id: bigint;
   vaceventForm: FormGroup;
   isUpdatingVacevent = false;
-  errors:{[key:string]:string} = {};
+  errors: { [key: string]: string } = {};
   datePipeStart: string;
   datePipeEnd: string;
   vaclocation: Vaclocation[];
   vacevent: Vacevent = VaceventFactory.empty();
-  state: string = "";
+  state: string = '';
 
-  constructor(private fb:FormBuilder, private vac:VaceventService, private route:ActivatedRoute, private router:Router, private datePipe: DatePipe, private vacloc: VaclocationService, private toastr:ToastrService) {
-   }
+  constructor(
+    private fb: FormBuilder,
+    private vac: VaceventService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private datePipe: DatePipe,
+    private vacloc: VaclocationService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit() {
-    
     const id = this.route.snapshot.params['id'];
     this.state = this.route.snapshot.params['state'];
-    if(id !== undefined){
+    if (id !== undefined) {
       this.isUpdatingVacevent = true;
       this.vac.getSingle(id).subscribe(vacevent => {
         this.vacevent = vacevent;
@@ -48,28 +53,37 @@ export class VaceventDetailsFormComponent implements OnInit {
     this.initVacevent();
   }
 
-  initVacevent(){
-      this.datePipeStart = this.datePipe.transform(this.vacevent.startTime, 'HH:mm');
-      this.datePipeEnd = this.datePipe.transform(this.vacevent.endTime, 'HH:mm');
-  
-      this.vaceventForm = this.fb.group({
-          id: this.vacevent.id,
-          vaclocation_id: [this.vacevent.vaclocation_id, Validators.required],
-          maxVac: [this.vacevent.maxVac, [Validators.required, Validators.min(1)]],
-          date: [this.vacevent.date, Validators.required],
-          startTime: [this.datePipeStart, Validators.required],
-          endTime: [this.datePipeEnd, Validators.required],
-      });
-    this.vaceventForm.statusChanges.subscribe(()=>{
+  initVacevent() {
+    this.datePipeStart = this.datePipe.transform(
+      this.vacevent.startTime,
+      'HH:mm'
+    );
+    this.datePipeEnd = this.datePipe.transform(this.vacevent.endTime, 'HH:mm');
+
+    this.vaceventForm = this.fb.group({
+      id: this.vacevent.id,
+      vaclocation_id: [this.vacevent.vaclocation_id, Validators.required],
+      maxVac: [this.vacevent.maxVac, [Validators.required, Validators.min(1)]],
+      date: [this.vacevent.date, Validators.required],
+      startTime: [this.datePipeStart, Validators.required],
+      endTime: [this.datePipeEnd, Validators.required]
+    });
+    this.vaceventForm.statusChanges.subscribe(() => {
       this.updateErrorMessages();
     });
   }
 
-  updateErrorMessages(){
+  updateErrorMessages() {
     this.errors = {};
-    for(const message of VaceventDetailsFormErrorMessages){
+    for (const message of VaceventDetailsFormErrorMessages) {
       const control = this.vaceventForm.get(message.forControl);
-      if(control && control.dirty && control.invalid && control.errors[message.forValidator] && !this.errors[message.forControl]){
+      if (
+        control &&
+        control.dirty &&
+        control.invalid &&
+        control.errors[message.forValidator] &&
+        !this.errors[message.forControl]
+      ) {
         this.errors[message.forControl] = message.text;
       }
     }
@@ -77,36 +91,41 @@ export class VaceventDetailsFormComponent implements OnInit {
 
   reloadCurrentRoute() {
     let currentUrl = this.router.url;
-    this.router.navigateByUrl('/', {skipLocationChange: true}).then(() => {
-        this.router.navigate([currentUrl]);
+    this.router.navigateByUrl('/', { skipLocationChange: true }).then(() => {
+      this.router.navigate([currentUrl]);
     });
   }
 
-  submitForm(){
+  submitForm() {
+    let updatedVacevent: Vacevent = VaceventFactory.fromObject(
+      this.vaceventForm.value
+    );
+    const startTimeNew = moment(
+      this.vaceventForm.value.date + ' ' + this.vaceventForm.value.startTime
+    ).toDate();
+    const endTimeNew = moment(
+      this.vaceventForm.value.date + ' ' + this.vaceventForm.value.endTime
+    ).toDate();
+    updatedVacevent.startTime = startTimeNew;
+    updatedVacevent.endTime = endTimeNew;
+    this.vacloc
+      .getLocationById(this.vaceventForm.controls['vaclocation_id'].value)
+      .subscribe(res => {
+        updatedVacevent.vaclocation = res;
+      });
 
-    let updatedVacevent:Vacevent = VaceventFactory.fromObject(this.vaceventForm.value);
-    console.log(this.vaceventForm.value.startTime);
-    const startTimeNew = moment(this.vaceventForm.value.date + ' ' + this.vaceventForm.value.startTime).toDate();
-    const endTimeNew = moment(this.vaceventForm.value.date + ' ' + this.vaceventForm.value.endTime).toDate();
-    updatedVacevent.startTime = startTimeNew; 
-    updatedVacevent.endTime = endTimeNew; 
-    this.vacloc.getLocationById(this.vaceventForm.controls['vaclocation_id'].value).subscribe(res  => {updatedVacevent.vaclocation = res;});
+    if (this.isUpdatingVacevent) updatedVacevent.users = this.vacevent.users;
+    else updatedVacevent.users = [];
 
-    if(this.isUpdatingVacevent)
-      updatedVacevent.users = this.vacevent.users;
-    else 
-      updatedVacevent.users = [];
-    
-    if(this.isUpdatingVacevent){
+    if (this.isUpdatingVacevent) {
       this.vac.update(updatedVacevent).subscribe(res => {
         this.toastr.success('Impftermin erfolgreich geändert');
         this.reloadCurrentRoute();
       });
     } else {
       this.vac.create(updatedVacevent).subscribe(res => {
-        this.router.navigate(["../../"],{relativeTo:this.route});
+        this.router.navigate(['../../'], { relativeTo: this.route });
       });
     }
   }
-
 }
